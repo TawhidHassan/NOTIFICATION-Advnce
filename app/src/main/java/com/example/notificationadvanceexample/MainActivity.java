ef21eb5758c3f6aebfbbb7482deb3a5520f917e9
sifat hassan <sifat007.sh7@gmail.com>
2021-04-16 20:25:57 +0600

    Create a Custom Notification

--- app/src/main/java/com/example/notificationadvanceexample/MainActivity.java
+++ app/src/main/java/com/example/notificationadvanceexample/MainActivity.java
@@ -3,131 +3,47 @@ package com.example.notificationadvanceexample;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
-import androidx.core.app.RemoteInput;
 
 import android.app.Notification;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.SystemClock;
-import android.support.v4.media.session.MediaSessionCompat;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.RemoteViews;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.example.notificationadvanceexample.APP.CHANNEL_1_ID;
-import static com.example.notificationadvanceexample.APP.CHANNEL_2_ID;
+import static com.example.notificationadvanceexample.APP.CHANNEL_ID;
 
 public class MainActivity extends AppCompatActivity {
     private NotificationManagerCompat notificationManager;
-    private EditText editTextTitle;
-    private EditText editTextMessage;
-    static List<Message> MESSAGES = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         notificationManager = NotificationManagerCompat.from(this);
-        editTextTitle = findViewById(R.id.edit_text_title);
-        editTextMessage = findViewById(R.id.edit_text_message);
-        MESSAGES.add(new Message("Good morning!", "Jim"));
-        MESSAGES.add(new Message("Hello", null));
-        MESSAGES.add(new Message("Hi!", "Jenny"));
     }
-    public void sendOnChannel1(View v) {
-        sendChannel1Notification(this);
-    }
-    public static void sendChannel1Notification(Context context) {
-        Intent activityIntent = new Intent(context, MainActivity.class);
-        PendingIntent contentIntent = PendingIntent.getActivity(context,
-                0, activityIntent, 0);
-        RemoteInput remoteInput = new RemoteInput.Builder("key_text_reply")
-                .setLabel("Your answer...")
-                .build();
-        Intent replyIntent;
-        PendingIntent replyPendingIntent = null;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            replyIntent = new Intent(context, DirectReplyReceiver.class);
-            replyPendingIntent = PendingIntent.getBroadcast(context,
-                    0, replyIntent, 0);
-        } else {
-            //start chat activity instead (PendingIntent.getActivity)
-            //cancel notification with notificationManagerCompat.cancel(id)
-        }
-        NotificationCompat.Action replyAction = new NotificationCompat.Action.Builder(
-                R.drawable.ic_baseline_4k_24,
-                "Reply",
-                replyPendingIntent
-        ).addRemoteInput(remoteInput).build();
-        NotificationCompat.MessagingStyle messagingStyle =
-                new NotificationCompat.MessagingStyle("Me");
-        messagingStyle.setConversationTitle("Group Chat");
-        for (Message chatMessage : MESSAGES) {
-            NotificationCompat.MessagingStyle.Message notificationMessage =
-                    new NotificationCompat.MessagingStyle.Message(
-                            chatMessage.getText(),
-                            chatMessage.getTimestamp(),
-                            chatMessage.getSender()
-                    );
-            messagingStyle.addMessage(notificationMessage);
-        }
-        Notification notification = new NotificationCompat.Builder(context, CHANNEL_1_ID)
+    public void showNotification(View v) {
+        RemoteViews collapsedView = new RemoteViews(getPackageName(),
+                R.layout.notification_collapsed);
+
+        RemoteViews expandedView = new RemoteViews(getPackageName(),
+                R.layout.notification_expanded);
+
+        Intent clickIntent = new Intent(this, NotificationReceiver.class);
+        PendingIntent clickPendingIntent = PendingIntent.getBroadcast(this,
+                0, clickIntent, 0);
+
+        collapsedView.setTextViewText(R.id.text_view_collapsed_1, "Hello World!");
+        expandedView.setImageViewResource(R.id.image_view_expanded, R.drawable.ic_baseline_4k_24);
+
+        expandedView.setOnClickPendingIntent(R.id.image_view_expanded, clickPendingIntent);
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_looks_one)
-                .setStyle(messagingStyle)
-                .addAction(replyAction)
-                .setColor(Color.BLUE)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
-                .setContentIntent(contentIntent)
-                .setAutoCancel(true)
-                .setOnlyAlertOnce(true)
+                .setCustomContentView(collapsedView)
+                .setCustomBigContentView(expandedView)
+                //.setStyle(new NotificationCompat.DecoratedCustomViewStyle())
                 .build();
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
         notificationManager.notify(1, notification);
-    }
-    public void sendOnChannel2(View v) {
-        String title1 = "Title 1";
-        String message1 = "Message 1";
-        String title2 = "Title 2";
-        String message2 = "Message 2";
-        Notification notification1 = new NotificationCompat.Builder(this, CHANNEL_2_ID)
-                .setSmallIcon(R.drawable.ic_baseline_4k_24)
-                .setContentTitle(title1)
-                .setContentText(message1)
-                .setPriority(NotificationCompat.PRIORITY_LOW)
-                .setGroup("example_group")
-                .build();
-        Notification notification2 = new NotificationCompat.Builder(this, CHANNEL_2_ID)
-                .setSmallIcon(R.drawable.ic_looks_one)
-                .setContentTitle(title2)
-                .setContentText(message2)
-                .setPriority(NotificationCompat.PRIORITY_LOW)
-                .setGroup("example_group")
-                .build();
-        Notification summaryNotification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
-                .setSmallIcon(R.drawable.ic_looks_one)
-                .setStyle(new NotificationCompat.InboxStyle()
-                        .addLine(title2 + " " + message2)
-                        .addLine(title1 + " " + message1)
-                        .setBigContentTitle("2 new messages")
-                        .setSummaryText("user@example.com"))
-                .setPriority(NotificationCompat.PRIORITY_LOW)
-                .setGroup("example_group")
-                .setGroupAlertBehavior(NotificationCompat.GROUP_ALERT_CHILDREN)
-                .setGroupSummary(true)
-                .build();
-        SystemClock.sleep(2000);
-        notificationManager.notify(2, notification1);
-        SystemClock.sleep(2000);
-        notificationManager.notify(3, notification2);
-        SystemClock.sleep(2000);
-        notificationManager.notify(4, summaryNotification);
     }
 }
--- app/src/main/java/com/example/notificationadvanceexample/NotificationReceiver.java
+++ app/src/main/java/com/example/notificationadvanceexample/NotificationReceiver.java
@@ -5,10 +5,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
+import androidx.core.app.NotificationManagerCompat;
+
 public class NotificationReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         String message = intent.getStringExtra("toastMessage");
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.cancel(1);
     }
 }
