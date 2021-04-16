52f2e7957b75212fef7b56e06324dc8ce4811aa3
sifat hassan <sifat007.sh7@gmail.com>
2021-04-16 19:38:21 +0600

    PROGRESS BAR NOTIFICATION

--- app/src/main/java/com/example/notificationadvanceexample/MainActivity.java
+++ app/src/main/java/com/example/notificationadvanceexample/MainActivity.java
@@ -14,6 +14,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v4.media.session.MediaSessionCompat;
 import android.view.View;
 import android.widget.EditText;
@@ -29,9 +30,6 @@ public class MainActivity extends AppCompatActivity {
     private EditText editTextTitle;
     private EditText editTextMessage;
 
-    static List<Message> MESSAGES = new ArrayList<>();
-
-    private MediaSessionCompat mediaSession;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,100 +39,40 @@ public class MainActivity extends AppCompatActivity {
         editTextTitle = findViewById(R.id.edit_text_title);
         editTextMessage = findViewById(R.id.edit_text_message);
 
-        mediaSession = new MediaSessionCompat(this, "tag");
-
-        MESSAGES.add(new Message("Good morning!", "Jim"));
-        MESSAGES.add(new Message("Hello", null));
-        MESSAGES.add(new Message("Hi!", "Jenny"));
     }
 
     public void sendOnChannel1(View v) {
-        sendChannel1Notification(this);
+
     }
-    public static void sendChannel1Notification(Context context) {
-        Intent activityIntent = new Intent(context, MainActivity.class);
-        PendingIntent contentIntent = PendingIntent.getActivity(context,
-                0, activityIntent, 0);
 
-        RemoteInput remoteInput = new RemoteInput.Builder("key_text_reply")
-                .setLabel("Your answer...")
-                .build();
-
-        Intent replyIntent;
-        PendingIntent replyPendingIntent = null;
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            replyIntent = new Intent(context, DirectReplyReceiver.class);
-            replyPendingIntent = PendingIntent.getBroadcast(context,
-                    0, replyIntent, 0);
-        } else {
-            //start chat activity instead (PendingIntent.getActivity)
-            //cancel notification with notificationManagerCompat.cancel(id)
-        }
-        NotificationCompat.Action replyAction = new NotificationCompat.Action.Builder(
-                R.drawable.ic_baseline_4k_24,
-                "Reply",
-                replyPendingIntent
-        ).addRemoteInput(remoteInput).build();
-        NotificationCompat.MessagingStyle messagingStyle =
-                new NotificationCompat.MessagingStyle("Me");
-        messagingStyle.setConversationTitle("Group Chat");
-        for (Message chatMessage : MESSAGES) {
-            NotificationCompat.MessagingStyle.Message notificationMessage =
-                    new NotificationCompat.MessagingStyle.Message(
-                            chatMessage.getText(),
-                            chatMessage.getTimestamp(),
-                            chatMessage.getSender()
-                    );
-            messagingStyle.addMessage(notificationMessage);
-        }
-        Notification notification = new NotificationCompat.Builder(context, CHANNEL_1_ID)
-                .setSmallIcon(R.drawable.ic_baseline_4k_24)
-                .setStyle(messagingStyle)
-                .addAction(replyAction)
-                .setColor(Color.BLUE)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
-                .setContentIntent(contentIntent)
-                .setAutoCancel(true)
-                .setOnlyAlertOnce(true)
-                .build();
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
-        notificationManager.notify(1, notification);
-    }
 
     public void sendOnChannel2(View v) {
-        String title = editTextTitle.getText().toString();
-        String message = editTextMessage.getText().toString();
-
-        Bitmap artwork = BitmapFactory.decodeResource(getResources(), R.drawable.sifat);
-
-
-        Notification notification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
-                .setSmallIcon(R.drawable.ic_baseline_4k_24)
-                .setContentTitle(title)
-                .setContentText(message)
-                .setLargeIcon(artwork)
-//                .setStyle(new NotificationCompat.InboxStyle()
-//                        .addLine("This is line 1")
-//                        .addLine("This is line 2")
-//                        .addLine("This is line 3")
-//                        .addLine("This is line 4")
-//                        .addLine("This is line 5")
-//                        .addLine("This is line 6")
-//                        .addLine("This is line 7")
-//                        .setBigContentTitle("Big Content Title")
-//                        .setSummaryText("Summary Text"))
-                .addAction(R.drawable.ic_baseline_4k_24, "Dislike", null)
-                .addAction(R.drawable.ic_launcher_foreground, "Previous", null)
-                .addAction(R.drawable.ic_baseline_4k_24, "Pause", null)
-                .addAction(R.drawable.ic_baseline_4k_24, "Next", null)
-                .addAction(R.drawable.ic_baseline_4k_24, "Like", null)
-                .setStyle(new androidx.media.app.NotificationCompat.MediaStyle()
-                        .setShowActionsInCompactView(1, 2, 3)
-                        .setMediaSession(mediaSession.getSessionToken()))
+        final int progressMax = 100;
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
+                .setSmallIcon(R.drawable.ic_looks_one)
+                .setContentTitle("Download")
+                .setContentText("Download in progress")
                 .setPriority(NotificationCompat.PRIORITY_LOW)
-                .build();
-        notificationManager.notify(2, notification);
+                .setOngoing(true)
+                .setOnlyAlertOnce(true)
+                .setProgress(progressMax, 0, false);
+        notificationManager.notify(2, notification.build());
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                SystemClock.sleep(2000);
+                for (int progress = 0; progress <= progressMax; progress += 20) {
+                    notification.setProgress(progressMax, progress, false);
+                    notificationManager.notify(2, notification.build());
+                    SystemClock.sleep(1000);
+                }
+                notification.setContentText("Download finished")
+                        .setProgress(0, 0, false)
+                        .setOngoing(false);
+                notificationManager.notify(2, notification.build());
+            }
+        }).start();
+
     }
 }
